9a3b5a3f91729d5f819a2e1938cc2653e4c51df5
Samuel <samuarcher@gmail.com>
2019-05-04 13:48:57 +0200

    Ajout projet

---
+++ ClientDashbord/BaseNotify.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ClientDashbord.Annotations;
+
+namespace ClientDashbord
+{
+	public class BaseNotify : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
